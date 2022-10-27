cf3d3e3c0f20b9acc45fbab602c132b70df27e04
Henry Sullivan <sullivanbhenry@gmail.com>
2022-10-27 16:02:33 -0700

    application now takes input, removes words less than three letters, reverses the order of words, and displays it back to user

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,35 @@
-function handleInput(event) {
-  event.preventDefault();
-  const userInput = document.getElementById("sentences").value
-  const userArray = userInput.split(" ");
-  console.log(userArray);
+//Business Logic
 
-  userArray.forEach(function(element) {
-    
-  })
+function userInput(text) {
+  userArray = text.split(" ");
+  newArray = [];
+  userArray.forEach(function(element){
+    if (element.length >= 3){
+      newArray.push(element);
+      return newArray
+    }
+  });
+       
+    newArray.reverse();     
+    finalArray = newArray.join(" ");
+    return finalArray
 }
 
 
+
+
+
+
+//UI Logic
+
+function gatherInput(event) {
+  event.preventDefault();
+  const sentence = document.getElementById("sentences").value
+
+  document.getElementById("newSentence").innerText = userInput(sentence)
+}
+
 window.addEventListener("load", function() {
- form = document.getElementById("form");
- form.addEventListener("submit", handleInput);
+  const form = document.getElementById("form");
+  form.addEventListener("submit", gatherInput);
 })
